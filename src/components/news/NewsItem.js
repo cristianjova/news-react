f3d2ec0bb6f8a0c90cacf4a760daebaaac10ae03
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-07-27 03:42:31 -0300

    loading images if available

--- src/components/news/NewsItem.js
+++ src/components/news/NewsItem.js
@@ -4,12 +4,23 @@ const NewsItem = ({ newsitem }) => {
   // Extraer los datos
   const { urlToImage, url, title, description, source } = newsitem;
 
+  // Load image if available
+  const image = urlToImage ? (
+    <div className='card-image'>
+      <img src={urlToImage} alt={title} />
+      <span
+        className='card-title white-text'
+        style={{ textShadow: '3px 1px 4px black' }}
+      >
+        {source.name}
+      </span>
+    </div>
+  ) : null;
+
   return (
     <div className='col s12 m6 l4'>
       <div className='card'>
-        <div className='card-image'>
-          <img src={urlToImage} alt={title} />
-        </div>
+        {image}
 
         <div className='card-content'>
           <h3>{title}</h3>
