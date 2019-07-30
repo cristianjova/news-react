52c989ee9973f6b3a0524b44b99ccb3c890d74c1
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-07-29 22:43:15 -0300

    Updates

---
+++ precache-manifest.f9df0e49b63d4f24e226f8a0c7ec9fca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90736660400466a46ae5ea016fa7ae7",
+    "url": "/news-react/index.html"
+  },
+  {
+    "revision": "810700595842bef24ae4",
+    "url": "/news-react/static/css/main.e90affcb.chunk.css"
+  },
+  {
+    "revision": "527e7009e4b618fd2321",
+    "url": "/news-react/static/js/2.74ca5632.chunk.js"
+  },
+  {
+    "revision": "810700595842bef24ae4",
+    "url": "/news-react/static/js/main.f24b32fc.chunk.js"
+  },
+  {
+    "revision": "a2975f5779d33fbe64c3",
+    "url": "/news-react/static/js/runtime~main.c6a7679f.js"
+  }
+]);
